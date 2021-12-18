fa02544d33841323b274843749023e9617b6bbfc
ParkByeolYi <51067412+ParkByeolYi@users.noreply.github.com>
2021-12-19 01:11:08 +0900

    Add files via upload
    --211219 작업내용
    DataFlow에 맞게 수정
    현재 되는 기능
    1. read users
    2. create user
    3. 아이디 별로 read user
    4. read scrap for user
    5. create scrap for user
    
    --내일 할 기능
    delete 구현
    ai scrap 구현
    html로 구현

--- scrap_app_by/sql_app/models.py
+++ scrap_app_by/sql_app/models.py
@@ -1,60 +1,3 @@
-# from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-# from sqlalchemy.orm import relationship
-
-# from .database import Base
-
-
-# # 회원 : 아이디(PK) 비밀번호 이름 알람설정
-# class User(Base):
-
-#     # 테이블의 이름
-#     __tablename__ = "users"
-
-#     id = Column(Integer, primary_key=True, index=True) # index ?
-#     hashed_password = Column(String)
-#     name = Column(String, index=True)
-#     alarm = Column(Boolean, default=True) # 일단 Boolean
-
-#     scraps = relationship("Scrap", back_populates="owner_user")
-
-
-# # 관리자 : 아이디(PK) 비밀번호
-# class Admin(Base):
-
-#     __tablename__ = "admins"
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     hashed_password = Column(String)
-
-
-# # 뉴스 : 아이디(PK) 제목 내용 스크랩(FK)
-# class News(Base):
-
-#     __tablename__ = "newss"
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     title = Column(String, index=True)
-#     article = Column(String, index=True)
-#     owner_scrap_id = Column(Integer, ForeignKey("scraps.id"))
-    
-#     owner_scrap = relationship("Scrap", back_populates="newss")
-
-
-# # 스크랩 : 아이디(PK) 질문뉴스_아이디(FK) 질문문장 답변뉴스_아이디(FK) 답변문장 작성자(FK)
-# class Scrap(Base):
-
-#     __tablename__ = "scraps"
-
-#     id = Column(Integer, primary_key=True, index=True)
-#     question_news_id = Column(Integer, index=True)
-#     question_sentence = Column(String, index=True)
-#     answer_news_id = Column(Integer, index=True)
-#     answer_sentence = Column(String, index=True)
-#     owner_user_id = Column(Integer, ForeignKey("users.id"))
-
-#     owner_user = relationship("User", back_populates="scraps")
-#     newss = relationship("News", back_populates="owner_scrap")
-
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
@@ -100,7 +43,7 @@ class NewsScrap(Base):
 
     __tablename__ = "news_scraps"
 
-    news_scrap_id = Column(Integer, primary_key=True, index=True)
+    news_scrap_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     user_id = Column(String, ForeignKey("users.user_id"))
     user_news_id = Column(Integer, ForeignKey("user_newss.user_news_id"))
 
@@ -111,7 +54,7 @@ class UserInput(Base):
 
     __tablename__ = "user_input"
 
-    user_input_id = Column(Integer, primary_key=True, index=True)
+    user_input_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     user_id = Column(String, ForeignKey("users.user_id"))
     user_news_id = Column(Integer, ForeignKey("user_newss.user_news_id"))
     user_input = Column(String, index=True)
@@ -135,8 +78,8 @@ class AIInput(Base):
 
     __tablename__ = "ai_inputs"
 
-    ai_input_id = Column(Integer, primary_key=True, index=True)
+    ai_input_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     user_id = Column(String, ForeignKey("users.user_id"))
     ai_news_id = Column(Integer, ForeignKey("user_newss.user_news_id"))
     ai_input = Column(String, index=True)
-    
+
--- scrap_app_by/sql_app/schemas.py
+++ scrap_app_by/sql_app/schemas.py
@@ -1,84 +1,3 @@
-# from typing import List, Optional
-# from pydantic import BaseModel
-
-
-# # Create에 id도 들어가야하나?
-# # 관리자 아이디가 뉴스에 들어가야하나?
-
-
-# # 관리자 : 아이디(PK) 비밀번호
-# class AdminBase(BaseModel):
-#     id: int
-
-
-# class AdminCreate(AdminBase):
-#     password: str
-
-
-# class Admin(AdminBase):
-#     pass
-
-#     class Config:
-#         orm_mode = True
-
-
-# # 뉴스 : 아이디(PK) 제목 내용
-# class NewsBase(BaseModel):
-#     title: str
-#     article: str
-
-
-# class NewsCreate(NewsBase):
-#     pass
-
-
-# class News(NewsBase):
-#     id: int
-#     owner_scrap_id: int
-
-#     class Config:
-#         orm_mode = True
-
-
-# # 스크랩 : 아이디(PK) 작성자(FK) 질문뉴스_아이디(FK) 질문문장 답변뉴스_아이디(FK) 답변문장
-# class ScrapBase(BaseModel):
-#     question_sentence: str
-#     answer_sentence: Optional[str] = None
-
-
-# class ScrapCreate(ScrapBase):
-#     pass
-
-
-# # ! 뉴스 id
-# class Scrap(ScrapBase):
-#     id: int
-#     question_news: List[News] = []
-#     answer_news: List[News] = []
-#     owner_user_id: int
-
-#     class Config:
-#         orm_mode = True
-
-
-# # 회원 : 아이디(PK) 비밀번호 이름 알람설정
-# class UserBase(BaseModel):
-#     id: int
-
-
-# class UserCreate(UserBase):
-#     password: str
-
-
-# class User(UserBase):
-#     name: Optional[str]
-#     alarm: Optional[bool] = False
-#     scraps: List[Scrap] = []
-
-#     class Config:
-#         orm_mode = True
-
-
 from typing import List, Optional
 from pydantic import BaseModel
 
@@ -121,15 +40,19 @@ class UserNews(UserNewsBase):
 # NewsScrap : 유저아이디(FK) 유저가보는뉴스아이디(FK)
 # 유저가 뉴스 스크랩한 뉴스
 class NewsScrapBase(BaseModel):
-    pass
-
+    news_scrap_id: int
+    user_news_id: int
 
 class NewsScrapCreate(NewsScrapBase):
     pass
 
 
-class NewsScrap(NewsScrapBase):
+class NewsScrapDelete(NewsScrapBase):
     pass
+
+
+class NewsScrap(NewsScrapBase):
+    user_id: str
     
     class Config:
         orm_mode = True
@@ -154,27 +77,29 @@ class UserInput(UserInputBase):
         orm_mode = True
 
 
-# AINewsScrap : 유저아이디(FK) AI가보는뉴스아이디(FK)
-# AI가 스크랩한 뉴스
-class AINewsScrapBase(BaseModel):
-    user_id: str
-    ai_news_id: int
+# DataFlow 상에서 삭제됨
+# # AINewsScrap : 유저아이디(FK) AI가보는뉴스아이디(FK)
+# # AI가 스크랩한 뉴스
+# class AINewsScrapBase(BaseModel):
+#     user_id: str
+#     ai_news_id: int
 
 
-class AINewsScrapCreate(AINewsScrapBase):
-    pass
+# class AINewsScrapCreate(AINewsScrapBase):
+#     pass
 
 
-class AINewsScrap(AINewsScrapBase):
-    pass
+# class AINewsScrap(AINewsScrapBase):
+#     pass
     
-    class Config:
-        orm_mode = True
+#     class Config:
+#         orm_mode = True
 
 
 # AIInput : 유저아이디(FK) AI가보는뉴스아이디(FK) AI답변문장
 # AI가 내놓은 답변
 class AIInputBase(BaseModel):
+    news_scrap_id: int
     user_id: str
     ai_news_id: int
     ai_input: str
@@ -196,15 +121,17 @@ class UserBase(BaseModel):
     user_id: str
 
 
+class UserDelete(UserBase):
+    password: str
+
+
 class UserCreate(UserBase):
     password: str
 
 
 class User(UserBase):
     name: Optional[str]
-    alarm: Optional[bool]
-    news_scraps: List[NewsScrap] = [] # 유저 스크랩 페이지에서 볼 것이기 때문에
-    ai_news_scraps: List[AINewsScrap] = [] # AI 스크랩 페이지에서 볼 것이기 때문에
+    alarm: Optional[bool] = False
 
     class Config:
         orm_mode = True
