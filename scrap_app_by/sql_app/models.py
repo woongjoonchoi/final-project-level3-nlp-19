de1a1a3035b459eecf4be1e1660d2c337757f4bc
ParkByeolYi <51067412+ParkByeolYi@users.noreply.github.com>
2021-12-18 21:40:40 +0900

    Update models.py
    --주석 및 오류 수정

--- scrap_app_by/sql_app/models.py
+++ scrap_app_by/sql_app/models.py
@@ -62,7 +62,7 @@ from sqlalchemy.orm import relationship
 from .database import Base
 
 
-# 회원 : 회원아이디(PK) 비밀번호 이름 알람설정
+# User : 유저아이디(PK) 비밀번호 이름 알람설정
 class User(Base):
 
     # 테이블의 이름
@@ -73,8 +73,12 @@ class User(Base):
     name = Column(String, index=True)
     alarm = Column(Boolean, default=False) # 일단 Boolean
 
+    # 각 페이지 생성으로 생각하시면 됩니다
+    news_scraps = relationship("NewsScrap", back_populates="owner_user2")
+    ai_news_scraps = relationship("AINewsScrap", back_populates="owner_user3")
 
-# 관리자 : 관리자아이디(PK) 비밀번호
+
+# Admin : 관리자아이디(PK) 비밀번호
 class Admin(Base):
 
     __tablename__ = "admins"
@@ -83,7 +87,7 @@ class Admin(Base):
     hashed_password = Column(String)
 
 
-# UserNews : 유저뉴스아이디(PK) 제목 내용 admin아이디(FK)
+# UserNews : 유저와AI가보는뉴스아이디(PK) 제목 내용 관리자아이디(FK)
 # 모든 유저가 볼 수 있는 뉴스
 class UserNews(Base):
 
@@ -92,46 +96,52 @@ class UserNews(Base):
     user_news_id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     article = Column(String, index=True)
-    admin_id = Column(String, ForeignKey("admins.id")) # 옵션 처리!
+    admin_id = Column(String, ForeignKey("admins.admin_id"))
 
 
-# UserInput : 유저질문문장 유저아이디(FK) 뉴스아이디(FK)
-# 유저가 입력한 question 관리
-class UserInput(Base):
-
-    __tablename__ = "user_input"
-
-    user_id = Column(String, ForeignKey("users.id"))
-    user_news_id = Column(Integer, ForeignKey("user_newss.id"))
-    user_input = Column(String, index=True)
-
-
-# NewsScrap : 유저아이디(FK) 유저뉴스아이디(FK)
+# NewsScrap : 유저아이디(FK) 유저가보는뉴스아이디(FK)
 # 유저가 뉴스 스크랩한 뉴스
 class NewsScrap(Base):
 
     __tablename__ = "news_scraps"
 
-    user_id = Column(String, ForeignKey("users.id"))
-    user_news_id = Column(Integer, ForeignKey("user_newss.id"))
+    user_id = Column(String, ForeignKey("users.user_id"))
+    user_news_id = Column(Integer, ForeignKey("user_newss.user_news_id"))
+
+    owner_user2 = relationship("User", back_populates="news_scraps")
 
 
-# AINewsScrap : 유저아이디(FK) AI뉴스아이디(FK)
+# UserInput : 유저아이디(FK) 유저가보는뉴스아이디(FK) 유저질문문장
+# 유저가 입력한 question 관리
+class UserInput(Base):
+
+    __tablename__ = "user_input"
+
+    user_id = Column(String, ForeignKey("users.user_id"))
+    user_news_id = Column(Integer, ForeignKey("user_newss.user_news_id"))
+    user_input = Column(String, index=True)
+
+
+# AINewsScrap : 유저아이디(FK) AI가보는뉴스아이디(FK)
 # AI가 스크랩한 뉴스
 class AINewsScrap(Base):
 
     __tablename__ = "ai_news_scraps"
 
-    user_id = Column(String, ForeignKey("users.id"))
-    ai_news_id = Column(Integer, ForeignKey("user_newss.id"))
+    user_id = Column(String, ForeignKey("users.user_id"))
+    ai_news_id = Column(Integer, ForeignKey("user_newss.user_news_id"))
+
+    owner_user3 = relationship("User", back_populates="ai_news_scraps")
 
 
-# AIInput : AI답변문장 유저아이디(FK) AI뉴스아이디(KF)
+# AIInput : 유저아이디(FK) AI가보는뉴스아이디(FK) AI답변문장
 # AI가 내놓은 답변
 class AIInput(Base):
 
     __tablename__ = "ai_inputs"
 
+    user_id = Column(String, ForeignKey("users.user_id"))
+    ai_news_id = Column(Integer, ForeignKey("user_newss.user_news_id"))
     ai_input = Column(String, index=True)
-    user_id = Column(String, ForeignKey("users.id"))
-    ai_news_id = Column(Integer, ForeignKey("user_newss.id"))
+
+    
