81cafc6c77f5e542d75f9c4980126ebb39a18852
woongjoonchoi <oongjoon10@gmail.com>
2021-12-19 20:14:59 +0900

    <feat> package directory management

---
+++ serving/services/aiscrappedboard.py
@@ -0,0 +1,7 @@
+
+
+# AI가 스크랩한 뉴스기사 목록 불러오기
+class Aiscrappedboard():
+
+    # AI가 스크랩한 뉴스기사 제목 리스트 가져오기
+    pass
---
+++ serving/services/batchserving.py
@@ -0,0 +1,5 @@
+
+
+# Batch serving 하기 : 뉴스 기사를 업로드하기, 사용자 질문 정보 불러오기, 모델로 예측하기, 결과값 저장하기
+class Batchserving():
+    pass
