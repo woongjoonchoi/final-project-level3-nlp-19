e9a2d53bfc36e1d818ade7440232886a5c13b144
woongjoonchoi <oongjoon10@gmail.com>
2021-12-20 13:57:44 +0900

    <recover> traincode recover
    recover from wjc2 branch

--- code/trainer_qa.py
+++ code/trainer_qa.py
@@ -77,6 +77,7 @@ class QuestionAnsweringTrainer(Trainer):
         self.control = self.callback_handler.on_evaluate(
             self.args, self.state, self.control, metrics
         )
+        wandb.log({"eval" : metrics})
         return metrics
 
     def predict(self, test_dataset, test_examples, ignore_keys=None):
