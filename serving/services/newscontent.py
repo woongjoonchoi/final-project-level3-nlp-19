558dd94dbf5d36972fb76d14c5369bf109480259
ParkByeolYi <51067412+ParkByeolYi@users.noreply.github.com>
2021-12-22 02:42:35 +0900

    <modify> news page
    news page update

--- serving/services/newscontent.py
+++ serving/services/newscontent.py
@@ -9,7 +9,8 @@ es = Elasticsearch()
 class Newscontent():
 
     # 뉴스 기사 내용과 사용자 입력 내용 불러오기
-    def get_news(db: Session, news_id: str, user_id: str):
+    def get_news(db: Session, news_id: str):
         
         res = es.get(index = "news_wiki_index_update", id=news_id)
-        return res["_source"], news_id
+        print(res)
+        return res["_source"]["title"], res["_source"]["article"]
